8d084343d55349055344c9488a594445e6f048c1
kimicol <remaud_a@epita.fr>
2015-02-08 15:53:33 +0100

    Redépart à zéro
    Effacement des précédentes classes pour mieux repartir

---
+++ jeu/Assets/joueur/controlplayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controlplayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.up * 10f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(Vector3.down * 10f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.right * 10f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.left * 10f * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(-Vector3.back * Time.deltaTime * 10f);
+        }
+	}
+}
