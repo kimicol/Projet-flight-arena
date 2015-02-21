65f1e7b844963226b74b81f9520dc375308a4670
kimicol <remaud_a@epita.fr>
2015-02-21 23:30:10 +0100

    Début menu
    Affichage d'un premier bouton dans le menu (tuto à suivre sur internet)

---
+++ jeu/Assets/menus/mainmenu_script.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainmenu_script : MonoBehaviour 
+{
+    private int choix_menu = 1;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnGUI()
+    {
+        switch(choix_menu)
+        {
+            case 1:
+                menu_principal();
+                break;
+        }
+    }
+
+    void menu_principal()
+    {
+        if(GUI.Button(new Rect(Screen.width/2-200, Screen.height/2, 400, 25), "Commencer"))
+        {
+
+        }
+    }
+}
