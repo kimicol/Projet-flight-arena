8ce753de566f6433763d9d1c9155d1ba94a03838
kimicol <remaud_a@epita.fr>
2015-03-08 12:51:52 +0100

    menu option
    ajout du son dans le menu option

--- jeu/Assets/menus/mainmenu_script.cs
+++ jeu/Assets/menus/mainmenu_script.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class mainmenu_script : MonoBehaviour 
 {
     private int choix_menu = 1;
+    private int volume = 5;
 
 	// Use this for initialization
 	void Start () 
@@ -57,6 +58,11 @@ public class mainmenu_script : MonoBehaviour
 
     void menu_options()
     {
+        volume = (int)GUI.HorizontalSlider(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 50, 100, 30), (float)volume, 0.0f, 10.0f);
+        GUI.Label(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 80, 100, 30), ("Volume : " + volume));
+
+        Camera.main.audio.volume = volume;
+
         if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2, 400, 25), "Retour"))
         {
             choix_menu = 1;
