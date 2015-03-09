9110efa3091cb8f1c9fb0fedd943470ea792513e
kimicol <remaud_a@epita.fr>
2015-03-09 13:00:52 +0100

    ajout destruction vaisseau

--- jeu/Assets/joueur/controlplayer.cs
+++ jeu/Assets/joueur/controlplayer.cs
@@ -1,35 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class controlplayer : MonoBehaviour {
+public class controlplayer : MonoBehaviour 
+{
+    private float v_max = 200f;
+    private float vitesse = 0f;
+    private float v_acceleration = 5f;
+
+    private float r_max = 50f;
+    private float rota_hor = 0f;
+    private float rota_ver = 0f;
+    private float r_acceleration = 5f;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	    if(Input.GetKey(KeyCode.UpArrow))
+	    if(Input.GetKey(KeyCode.UpArrow) || (!Input.GetKey(KeyCode.DownArrow) &&rota_ver < 0f))
         {
-            transform.Rotate(Vector3.right * 50f * Time.deltaTime);
+            if(rota_ver < r_max)
+                rota_ver += r_acceleration; 
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.DownArrow) || rota_ver > 0f)
         {
-            transform.Rotate(Vector3.left * 50f * Time.deltaTime);
+            if(rota_ver > -r_max)
+                rota_ver -= r_acceleration;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        transform.Rotate(Vector3.right * rota_ver * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow) || (!Input.GetKey(KeyCode.LeftArrow) && rota_hor < 0f))
         {
-            transform.Rotate(Vector3.up * 50f * Time.deltaTime);
+            if(rota_hor < r_max)
+                rota_hor += r_acceleration;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow) || rota_hor > 0f)
         {
-            transform.Rotate(Vector3.down * 50f * Time.deltaTime);
+            if(rota_hor > -r_max)
+                rota_hor -= r_acceleration;
         }
-        if(Input.GetKey(KeyCode.Space))
+        transform.Rotate(Vector3.up * rota_hor * Time.deltaTime);
+
+        //Déplacement vers l'avant
+        if(Input.GetKey(KeyCode.Space) && vitesse < v_max)
         {
-            transform.Translate(-Vector3.back * Time.deltaTime * 200f);
+            vitesse += v_acceleration;
         }
+        else if(vitesse > 0)
+        {
+            vitesse -= v_acceleration;
+        }
+        transform.Translate(-Vector3.back * Time.deltaTime * vitesse);
 	}
 }
--- jeu/Assets/joueur/vie.cs
+++ jeu/Assets/joueur/vie.cs
@@ -4,6 +4,31 @@ using System.Collections;
 public class vie : MonoBehaviour {
 
     private int pv = 5;
+    public Animation anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        if(anim == null)
+        {
+            Debug.Log("test");
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Debug.Log("bla");
+            pv = 0;
+        }
+
+        if (pv <= 0 )//&& !anim.isPlaying)
+        {
+            //anim.Play();
+            DestroyObject(gameObject, 2);
+        }
+    }
 
     void OnTriggerEnter(Collider objet)
     {
@@ -13,11 +38,6 @@ public class vie : MonoBehaviour {
         {
             pv -= feu.degat;
             Destroy(feu);
-
-            if(pv <= 0)
-            {
-                Destroy(gameObject);
-            }
         }
     }
 }
