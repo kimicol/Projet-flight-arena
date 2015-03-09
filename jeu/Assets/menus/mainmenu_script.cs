c430421a8841bca0318186842247c9cacc107f6b
kimicol <remaud_a@epita.fr>
2015-03-09 17:08:32 +0100

    buildings et titre
    ajout dedeux immeubles.
    ajout du titre du jeu dans les menus

--- jeu/Assets/menus/mainmenu_script.cs
+++ jeu/Assets/menus/mainmenu_script.cs
@@ -26,6 +26,7 @@ public class mainmenu_script : MonoBehaviour
     void OnGUI()
     {
         GUI.skin = skin;
+        GUI.TextArea(new Rect(Screen.width / 4, Screen.height / 5, Screen.width / 2, Screen.height / 5), "Flight Arena");
         switch(choix_menu)
         {
             case 1:
@@ -43,24 +44,24 @@ public class mainmenu_script : MonoBehaviour
     /// </summary>
     void menu_principal()
     {
-        if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 60, 400, 25), "Un joueur"))
+        if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 30, 400, 25), "Un joueur"))
         {
             Application.LoadLevel("try");
         }
 
-        if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 30, 400, 25), "Multijoueur"))
+        if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2, 400, 25), "Multijoueur"))
         {
             // A MODIFIER
             Application.Quit();
         }
 
-        if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2, 400, 25), "Options"))
+        if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2 + 30, 400, 25), "Options"))
         {
             choix_menu = 2;
             return;
         }
 
-        if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2 + 30, 400, 25), "Quitter"))
+        if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2 + 60, 400, 25), "Quitter"))
         {
             Application.Quit();
         }
@@ -72,13 +73,13 @@ public class mainmenu_script : MonoBehaviour
     void menu_options()
     {
         //Gestion du son
-        volume = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 50, 100, 30), volume, 0.0f, 1.0f);
-        GUI.Label(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 80, 100, 30), ("Volume : " + (int)(10*volume)));
+        volume = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 200, Screen.height / 2 + 30, 100, 30), volume, 0.0f, 1.0f);
+        GUI.Label(new Rect(Screen.width / 2 - 200, Screen.height / 2, 100, 30), ("Volume : " + (int)(10*volume)));
 
         AudioListener.volume = volume;
         PlayerPrefs.SetFloat("Volume", volume);
 
-        if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2, 400, 25), "Retour"))
+        if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2 + 60, 400, 25), "Retour"))
         {
             choix_menu = 1;
             return;
