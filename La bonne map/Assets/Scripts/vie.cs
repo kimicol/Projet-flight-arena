7127fb9b3dedc585bf76e265fd0b7aeb3597f3fd
kimicol <remaud_a@epita.fr>
2015-03-11 16:43:38 +0100

    essais ratés

--- La bonne map/Assets/Scripts/vie.cs
+++ La bonne map/Assets/Scripts/vie.cs
@@ -4,35 +4,35 @@ using System.Collections;
 public class vie : MonoBehaviour {
 
     private int pv = 5;
-    public Animation anim;
+    //public Animation anim;
 
     void Start()
     {
-        anim = gameObject.GetComponent<Animation>();
+        /*anim = gameObject.GetComponent<Animation>();
         if(anim == null)
         {
             Debug.Log("test");
-        }
+        }*/
     }
 
     void Update()
     {
         if(Input.GetKey(KeyCode.Escape))
         {
-            Debug.Log("bla");
+            //Debug.Log("bla");
             pv = 0;
         }
 
         if (pv <= 0 )//&& !anim.isPlaying)
         {
             //anim.Play();
-            DestroyObject(gameObject, 2);
+            Destroy(this);
         }
     }
 
-    void OnTriggerEnter(Collider objet)
+    void OnTriggerEnter(MeshCollider collision)
     {
-        balle feu = collider.gameObject.GetComponent<balle>();
+        balle feu = collision.gameObject.GetComponent<balle>();
 
         if(feu != null)
         {
