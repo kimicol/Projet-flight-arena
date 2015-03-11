8d1bd76cf2190f7d512cbaf1ac2b13eecd2d14ac
kimicol <remaud_a@epita.fr>
2015-03-11 12:30:13 +0100

    Premier executable

---
+++ La bonne map/Assets/menus/decor_menu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class decor_menu : MonoBehaviour 
+{
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * 10);
+	}
+}
