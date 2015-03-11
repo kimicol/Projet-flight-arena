71a80e52473e57ae3d1b6ad28f9cbedd0620f549
kimicol <remaud_a@epita.fr>
2015-03-11 19:14:39 +0100

    Revert "test"
    This reverts commit 607d7d698b916d5b20ecb917630ca30eb0b7617d.

--- La bonne map/Assets/menus/mainmenu_script.cs
+++ La bonne map/Assets/menus/mainmenu_script.cs
@@ -26,7 +26,7 @@ public class mainmenu_script : MonoBehaviour
     void OnGUI()
     {
         GUI.skin = skin;
-        GUI.TextArea(new Rect(Screen.width / 4, Screen.height / 5, Screen.width / 2, Screen.height / 5), "Flight Arena");
+        GUI.TextArea(new Rect(Screen.width / 3, Screen.height / 5, Screen.width*3/4, Screen.height / 5), "Flight Arena");
         switch(choix_menu)
         {
             case 1:
@@ -46,7 +46,7 @@ public class mainmenu_script : MonoBehaviour
     {
         if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 30, 400, 25), "Un joueur"))
         {
-            Application.LoadLevel("try");
+            Application.LoadLevel(1);
         }
 
         if (GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2, 400, 25), "Multijoueur"))
