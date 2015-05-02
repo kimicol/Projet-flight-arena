60e296123f0dcef757e48deba63ab9599ed8ecb8
mastermat64 <mastermat64@hotmail.fr>
2015-05-02 15:04:19 +0200

    ajout elements gui
    clock finie
    vie non finie

---
+++ La bonne map/Assets/clock_gui.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+using System;
+
+public class clock_gui : MonoBehaviour {
+    private GUIText gui_timer;
+    private float startTime;
+    private int time_text;
+    private int limite_temps = 120; // en secondes |
+
+	// Use this for initialization
+	void Start () {
+        gui_timer = gameObject.GetComponents<GUIText>()[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OnGui();
+	}
+    private void OnGui()
+    {
+        time_text =limite_temps - Convert.ToInt32(Time.time - startTime);
+        gui_timer.text = time_text / 60 + ":" + time_text % 60;
+    }
+    private void Awake()
+    {
+        startTime = Time.time;
+    }
+}
---
+++ La bonne map/Assets/joueur/testGUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testGUI : MonoBehaviour {
+    private GUIText life_gui;
+    private int nb_pv;
+    private Vector2 position;
+
+	// Use this for initialization
+	void Start () {
+        life_gui = gameObject.GetComponents<GUIText>()[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OnGui();
+	}
+    private void OnGui()
+    {
+        life_gui.text = nb_pv.ToString();
+    }
+}
