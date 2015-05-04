ad2b4f1b28267b253b8c309c23de8195eedb1e59
kimicol <remaud_a@epita.fr>
2015-05-04 12:04:56 +0200

    multi debug 4

--- multi/Assets/Scripts/controlcamera.cs
+++ multi/Assets/Scripts/controlcamera.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Net;
 
 public class controlcamera : MonoBehaviour
 {
@@ -12,7 +13,7 @@ public class controlcamera : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () 
+    void Update()
     {
         if (vaisseau != null)
         {
@@ -20,5 +21,15 @@ public class controlcamera : MonoBehaviour
             this.transform.position = vaisseau.transform.position;
             this.transform.Translate(new Vector3(0f, 3f, -9f));
         }
-	}
+    }
+
+    void OnGUI()
+    {
+        if(Network.peerType == NetworkPeerType.Server)
+        {
+            string ip = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].ToString();
+
+            GUI.Label(new Rect(0, 0, 100, 30), ip);
+        }
+    }
 }
