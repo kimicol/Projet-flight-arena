8b5902d79f3dff034fe36bea7c19a67871e6bf84
mastermat64 <mastermat64@hotmail.fr>
2015-05-06 16:02:57 +0200

    fix apparition gameover

--- La bonne map/Assets/Scripts/controlcamera.cs
+++ La bonne map/Assets/Scripts/controlcamera.cs
@@ -20,7 +20,7 @@ public class controlcamera : MonoBehaviour
     }
 	
 	// Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if (vaisseau != null)
         {
