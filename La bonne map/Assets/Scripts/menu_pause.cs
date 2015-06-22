652982f357a3dff3d93cc88c0573d913ce166480
mastermat64 <mastermat64@hotmail.fr>
2015-06-22 20:57:13 +0200

    menu video ok (changer label to box a faire)

--- La bonne map/Assets/Scripts/menu_pause.cs
+++ La bonne map/Assets/Scripts/menu_pause.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 public class menu_pause : MonoBehaviour
 {
 
     int paused = 0;
-    int var = 0;
+    int temp = 4; // a changer 
+    int temp2;
     float volume;
     bool changed_sound = false;
     public GUISkin skin;
@@ -14,6 +16,21 @@ public class menu_pause : MonoBehaviour
     void Start()
     {
         volume = this.camera.audio.volume;
+        if (QualitySettings.anisotropicFiltering == AnisotropicFiltering.Disable)
+        {
+            temp2 = 0;
+        }
+        else
+        {
+            if (QualitySettings.anisotropicFiltering == AnisotropicFiltering.Enable)
+            {
+                temp2 = 1;
+            }
+            else
+            {
+                temp2 = 2;
+            }
+        }
     }
 
     void Update()
@@ -86,6 +103,7 @@ public class menu_pause : MonoBehaviour
                 volume = newLength;
             }
             string[] toolbarStrings = new string[] { "Mono", "Stereo", "Quad", "Surround", "5.1", "7.1"};
+            int var = 0;
             var = GUI.Toolbar(new Rect(Screen.width / 2 - 400, Screen.height / 2 + 55, 800, 25), var, toolbarStrings);
             switch (var)
             {
@@ -118,6 +136,54 @@ public class menu_pause : MonoBehaviour
         {
             GUI.Label(new Rect(Screen.width / 2 - 130, Screen.height / 6, Screen.width / 2, Screen.height / 5), "Pause");
 
+            GUI.Box(new Rect(Screen.width / 2 - 160, Screen.height / 2 - 150, 300,25), "Anisotropic Textures");
+            string[] toolbarStrings = new string[] { "Disable", "Enable", "ForceEnable" };
+            int var = temp;
+            var = GUI.Toolbar(new Rect(Screen.width / 2 - 400, Screen.height / 2 - 85, 800, 25), var, toolbarStrings);
+            switch (var)
+            {
+                case 0:
+                    QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
+                    break;
+                case 1:
+                    QualitySettings.anisotropicFiltering = AnisotropicFiltering.Enable;
+                    break;
+                case 2:
+                    QualitySettings.anisotropicFiltering = AnisotropicFiltering.ForceEnable;
+                    break;
+            }
+            temp = var;
+            var = temp2;
+            GUI.Label(new Rect(Screen.width / 2 - 150, Screen.height / 2 - 50, 900, Screen.height / 5), "Anti Aliasing");
+            toolbarStrings = new string[] {"0" ,"2x", "4x", "8x"};
+            var = GUI.Toolbar(new Rect(Screen.width / 2 - 400, Screen.height / 2 + 10, 800, 25), var, toolbarStrings);
+            switch (var)
+            {
+                case 0:
+                    QualitySettings.antiAliasing = 0;
+                    break;
+                case 1:
+                    QualitySettings.antiAliasing = 2;
+                    break;
+                case 2:
+                    QualitySettings.antiAliasing = 4;
+                    break;
+                case 3:
+                    QualitySettings.antiAliasing = 8;
+                    break;
+            }
+            temp2 = var;
+            bool coche = QualitySettings.softVegetation;
+            coche = GUI.Toggle(new Rect(Screen.width / 2 - 130, Screen.height / 2 + 50, 800, 25), coche, "Soft Particles");
+            QualitySettings.softVegetation = coche;
+            float v_number = 1;
+            string lengthText = GUI.TextField(new Rect(Screen.width / 2 - 150, Screen.height / 2 + 90, 300, 25), v_number.ToString());
+            float newLength;
+            if (float.TryParse(lengthText, out newLength))
+            {
+                v_number = newLength;
+            }
+            Debug.Log("AA : " + temp + " filtre : " + temp2);
         }
     }
 }
