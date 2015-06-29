11d032a50bd24442c61ceb43fee7748808a6b3f7
rosvinz <rosvinz@gmail.com>
2015-06-29 18:21:37 +0400

    maptutofinie

---
+++ La bonne map/Assets/Scripts/target_disappear.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class target_disappear : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	void OnTriggerEnter(Collider Collision)
+	{
+		DestroyObject(this.gameObject);
+	}
+}
